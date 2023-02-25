3136ea07894fe1f43d6a8e57864de38255049b4d
Petter Carranza <peterpaul.0194@gmail.com>
2023-02-25 17:18:36 -0500

    new jib config

--- src/main/java/com/bindord/eureka/gateway/controller/resourceserver/ProfessionController.java
+++ src/main/java/com/bindord/eureka/gateway/controller/resourceserver/ProfessionController.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
