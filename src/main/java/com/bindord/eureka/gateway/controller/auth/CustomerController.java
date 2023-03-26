b3b4f23bc2a30c87088c606803ef68e9963cd9ef
Panda <63072537+giovanny110@users.noreply.github.com>
2023-03-25 20:35:21 -0500

    HEP-143: Update Customer information and location

--- src/main/java/com/bindord/eureka/gateway/controller/auth/CustomerController.java
+++ src/main/java/com/bindord/eureka/gateway/controller/auth/CustomerController.java
@@ -2,12 +2,16 @@ package com.bindord.eureka.gateway.controller.auth;
 
 import com.bindord.eureka.auth.model.Customer;
 import com.bindord.eureka.auth.model.CustomerPersist;
+import com.bindord.eureka.resourceserver.model.CustomerInformationUpdateDto;
+import com.bindord.eureka.resourceserver.model.CustomerLocationUpdateDto;
+import com.bindord.eureka.resourceserver.model.CustomerPasswordUpdateDto;
 import com.bindord.eureka.gateway.wsc.AuthClientConfiguration;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +42,57 @@ public class CustomerController {
                 .body(Mono.just(customer), CustomerPersist.class)
                 .retrieve()
                 .bodyToMono(Customer.class)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @ApiResponse(description = "Update a customer information",
+            responseCode = "200")
+    @PutMapping(value = "/updateInformation",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<Customer> updateInformation(@Valid @RequestBody CustomerInformationUpdateDto customer) {
+        return authClientConfiguration.init()
+                .put()
+                .uri("/customer/updateAbout")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(customer), CustomerInformationUpdateDto.class)
+                .retrieve()
+                .bodyToMono(Customer.class)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @ApiResponse(description = "Update a customer information",
+            responseCode = "200")
+    @PutMapping(value = "/updateLocation",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<Boolean> updateLocation(@Valid @RequestBody CustomerLocationUpdateDto customer) {
+        return authClientConfiguration.init()
+                .put()
+                .uri("/customer/updateLocation")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(customer), CustomerLocationUpdateDto.class)
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @ApiResponse(description = "Update a customer information",
+            responseCode = "200")
+        @PutMapping(value = "/updatePassword",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<Boolean> updatePassword(@Valid @RequestBody CustomerPasswordUpdateDto customer) {
+        return authClientConfiguration.init()
+                .put()
+                .uri("/customer/updatePassword")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(customer), CustomerPasswordUpdateDto.class)
+                .retrieve()
+                .bodyToMono(Boolean.class)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 }
